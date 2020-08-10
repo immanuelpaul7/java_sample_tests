2c6d16c5ff9e8bdc79a06bc3b0c68701b4d16bcf
Immanuel Paul Perli <immanuel.perli@servicenow.com>
2020-08-10 10:37:18 -0700

    jenkins301

--- azureCommit.sh
+++ azureCommit.sh
@@ -1,9 +1,9 @@
 string=""
 x=1
-j=200
-while [ $x -le 100 ]
+j=300
+while [ $x -le 1500 ]
 do
-  string+="touch jenkins$((j+x)) && git add . && git commit -m jenkins$((j+x)) && sleep 1 && "
+  string+="touch jenkins$((j+x)) && git add . && git commit -m jenkins$((j+x)) && "
   x=$[$x+1]
 done
 
