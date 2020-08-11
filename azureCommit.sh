d10d3006cad0bdd85b01101ae63edd8e34ce09d5
Immanuel Paul Perli <immanuel.perli@servicenow.com>
2020-08-11 12:23:39 -0700

    jenkins70001

--- azureCommit.sh
+++ azureCommit.sh
@@ -1,13 +1,13 @@
 string=""
 x=1
-j=50000
-while [ $x -le 5000 ]
+j=70000
+while [ $x -le 3000 ]
 do
   string="touch jenkins$((j+x)) && git add . && git commit -m jenkins$((j+x)) "
   eval "$string"
   x=$[$x+1]
 done
 
-# string+="git push origin master"
+string="git push origin tb_1"
 echo $string
 eval "$string"
